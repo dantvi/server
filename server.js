13d7a08c5596a7de40c6c937cf30fad52fafd57a
dantvi <daniel.tving@gmail.com>
2025-03-07 15:25:37 +0100

    setting up security example

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+const path = require('path');
 const express = require('express');
+
+const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('this is working');
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!');
 });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
