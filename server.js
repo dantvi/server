14306480f9bd7776a410b07e30ec93668dfe42af
dantvi <daniel.tving@gmail.com>
2025-03-08 12:15:51 +0100

    adding helmet

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
   return res.send('Your personal secret value is 42!');
@@ -16,10 +19,13 @@ app.get('/', (req, res) => {
 });
 
 https
-  .createServer({
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
-  }, app)
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
